74470fa08dae03973271595e65aae3f4d7f9292c
charlainemacapallag-rgb <CharlaineM@gmail.com>
2025-12-09 04:09:43 +0800

    Initial commit - student api

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,15 +1,20 @@
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
-    info: { title: 'Student API', version: '1.0.0', description: 'Student API docs' },
-    servers: [{ url: '/' }]
+    openapi: "3.0.0",
+    info: {
+      title: "Student API",
+      version: "1.0.0",
+      description: "API documentation for your Student Management System",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
   },
-  apis: ['./routes/*.js']
+  apis: ["./routes/*.js"], // Scan all route files
 };
 
-const swaggerSpec = swaggerJsDoc(options);
-
-module.exports = { swaggerUi, swaggerSpec };
+module.exports = swaggerJsDoc(options);
